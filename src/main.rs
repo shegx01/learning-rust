d5e9d7d92e423b67a0389427c455ac60c0d3b017
O.O Samson <shegx01@gmail.com>
2020-07-14 19:02:01 +0100

    practicing variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+ use std::io;
+
 fn main() {
-    println!("Hello world")
+    // programme wont compile without semicolon
+    println!("please input your guess: ");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read input");
+
+    println!("Your guess is : {}", guess);
+    let mut foo = 5;
+    foo = foo + 10;
+    println!("The result of foo is: {}", foo);
 }
+
+// cargo with --bin arg allows makes an executable file ie binary
+// cargo build build a project
+// cargo run  execute a project
+// cargo check checks in case of errors
