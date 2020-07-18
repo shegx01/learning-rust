700ab843ad25dbb22d1f0f6b0072347925ad8c52
O.O Samson <shegx01@gmail.com>
2020-07-18 16:10:08 +0100

    TRPL chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
- use std::io;
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    // programme wont compile without semicolon
-    println!("please input your guess: ");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read input");
+    // programm wont compile without semicolon
 
-    println!("Your guess is : {}", guess);
-    let mut foo = 5;
-    foo = foo + 10;
-    println!("The result of foo is: {}", foo);
+    let secret_number = rand::thread_rng().gen_range(5, 20);
+
+
+    loop {
+        println!("please input your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_reason) => continue,
+        };
+
+        println!("Your guess is : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Gotcha");
+                break;
+
+            }
+        };
+    }
+
+
 }
 
 // cargo with --bin arg allows makes an executable file ie binary
