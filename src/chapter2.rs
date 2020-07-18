8f83dc7a65ce3784ed33559ccbfe5ca203410642
O.O Samson <shegx01@gmail.com>
2020-07-18 16:12:37 +0100

    moved to a single files structure

---
+++ src/chapter2.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn run() {
+    // programm wont compile without semicolon
+
+    let secret_number = rand::thread_rng().gen_range(5, 20);
+
+    loop {
+        println!("please input your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_reason) => continue,
+        };
+
+        println!("Your guess is : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Gotcha");
+                break;
+            }
+        };
+    }
+}
